6f0856561fd4119f4dc4b80414328c56e25c8e14
darienlakes2004 <112899977+darienlakes2004@users.noreply.github.com>
2022-10-08 19:12:53 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//is script
+console.log("hello");
+
+document.getElementById("btna").onclick = function() {
+	console.log("A")
+//	console.log("")
+}
+document.getElementById("btnd").onclick = function() {
+	console.log("D")
+//	console.log("")
+
+}
+document.getElementById("btnk").onclick = function() {
+	console.log("K")
+//	console.log("")
+}
